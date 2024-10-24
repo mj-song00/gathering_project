9bba623f962c364bb40179bdff30e28e4fb572ef
mj-song00 <niveus128@gmail.com>
2024-10-24 16:40:10 +0900

    Chore: category 관계 설정

--- src/main/java/com/sparta/gathering/domain/gather/entity/Gather.java
+++ src/main/java/com/sparta/gathering/domain/gather/entity/Gather.java
@@ -2,11 +2,13 @@ package com.sparta.gathering.domain.gather.entity;
 
 
 import com.sparta.gathering.common.entity.Timestamped;
+import com.sparta.gathering.domain.category.entity.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
@@ -21,8 +23,13 @@ public class Gather extends Timestamped {
 
     private LocalDateTime deletedAt;
 
-    public Gather(String title){
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Gather(String title, Category category){
         this.title = title;
+        this.category = category;
     }
 
     public void updateGatherTitle(String title){
