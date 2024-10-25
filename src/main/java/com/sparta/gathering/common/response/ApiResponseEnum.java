67ed618f530c6011467320856f7ba2cf165d34db
gyeol <gyeol9012@gmail.com>
2024-10-25 12:00:40 +0900

    Add : 프로필 이미지 수정,조회,삭제 구현

--- src/main/java/com/sparta/gathering/common/response/ApiResponseEnum.java
+++ src/main/java/com/sparta/gathering/common/response/ApiResponseEnum.java
@@ -8,6 +8,12 @@ public enum ApiResponseEnum {
     // 유저 관련
     SIGNUP_SUCCESS("회원가입이 완료되었습니다. 로그인 화면으로 이동합니다."),
     USER_DELETED_SUCCESS("회원 탈퇴가 완료되었습니다. 이용해 주셔서 감사합니다."),
+
+    // 유저 프로필 등록
+    USER_PROFILE_UPLOAD_SUCCESS("프로필 이미지가 정상적으로 등록되었습니다."),
+    USER_PROFILE_GET_SUCCESS("프로필 이미지가 정상적으로 조회되었습니다."),
+    USER_PROFILE_DELETE_SUCCESS("프로필 이미지가 정상적으로 삭제되었습니다."),
+
     // 카테고리 관련
     CREATED_CATEGORY_SUCCESS("카테고리가 성공적으로 생성되었습니다."),
     DELETED_CATEGORY_SUCCESS("카테고리가 성공적으로 삭제되었습니다."),
