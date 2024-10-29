fe833acc663fb008e6026d578559c6eb29f61177
LJH <skRtl365@gmail.com>
2024-10-29 18:40:13 +0900

    Refactor : 최신화 후 컨벤션 맞춰서 조정

--- src/main/java/com/sparta/gathering/domain/gather/controller/GatherController.java
+++ src/main/java/com/sparta/gathering/domain/gather/controller/GatherController.java
@@ -9,6 +9,7 @@ import com.sparta.gathering.domain.gather.entity.Gather;
 import com.sparta.gathering.domain.gather.service.GatherService;
 import com.sparta.gathering.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,9 +17,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Gather", description = "소모임 API")
 @RestController
--- src/main/java/com/sparta/gathering/domain/hashtag/entity/HashTag.java
+++ src/main/java/com/sparta/gathering/domain/hashtag/entity/HashTag.java
@@ -3,15 +3,22 @@ package com.sparta.gathering.domain.hashtag.entity;
 import com.sparta.gathering.common.entity.Timestamped;
 import com.sparta.gathering.domain.gather.entity.Gather;
 import com.sparta.gathering.domain.hashtag.dto.request.HashTagReq;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
 @Entity
@@ -42,11 +49,12 @@ public class HashTag extends Timestamped {
         this.gather = gather;
     }
 
-    public static HashTag from(HashTagReq hashTagReq,Gather gather) {
-        return new HashTag(hashTagReq.getHashTagName(), gather );
+    public static HashTag from(HashTagReq hashTagReq, Gather gather) {
+        return new HashTag(hashTagReq.getHashTagName(), gather);
     }
-    public static HashTag from(String HashTagName, Gather gather){
-        return new HashTag(HashTagName, gather);
+
+    public static HashTag from(String hashTagName, Gather gather) {
+        return new HashTag(hashTagName, gather);
     }
 
     public static HashTag of(String hashTagName, Gather gather) {
@@ -56,6 +64,5 @@ public class HashTag extends Timestamped {
     public void updateDeleteAt() {
         this.deletedAt = LocalDateTime.now();
     }
-
 
 }
