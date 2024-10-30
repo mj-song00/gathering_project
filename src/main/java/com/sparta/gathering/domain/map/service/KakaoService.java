308b6b806024cd1de9601be6198e870405b0d8a4
zerodeok <dudejr3165@naver.com>
2024-10-30 20:16:39 +0900

    del : jwt 이동및 , 환경변수 설정

--- src/main/java/com/sparta/gathering/domain/map/service/KakaoService.java
+++ src/main/java/com/sparta/gathering/domain/map/service/KakaoService.java
@@ -6,6 +6,7 @@ import com.sparta.gathering.domain.map.entity.Map;
 import com.sparta.gathering.domain.map.repository.MapRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,12 +21,16 @@ public class KakaoService {
     private final MapRepository mapRepository;
     private final ObjectMapper objectMapper;
 
+    @Value("${kakao.map.api-key}")
+    private String appKey;
+
     public ResponseEntity<String> searchMap(String searchMap) {
 
         // Set up headers
         headers.setContentType(MediaType.APPLICATION_JSON);
-        String appkey = "KakaoAK fe5907af108eb89d33ecb50f39844b0e";//
-        headers.set("Authorization", appkey);
+
+
+        headers.set("Authorization", appKey);
 
         // Create entity with headers
         HttpEntity<String> entity = new HttpEntity<>("parameters",headers);
@@ -37,8 +42,7 @@ public class KakaoService {
     public void saveMap(String saveMap) {
 
         headers.setContentType(MediaType.APPLICATION_JSON);
-        String appkey = "KakaoAK fe5907af108eb89d33ecb50f39844b0e";//
-        headers.set("Authorization", appkey);
+        headers.set("Authorization", appKey);
 
         HttpEntity<String> entity = new HttpEntity<>("parameters",headers);
         String rawURI = "https://dapi.kakao.com/v2/local/search/address.json?query=" + saveMap;
