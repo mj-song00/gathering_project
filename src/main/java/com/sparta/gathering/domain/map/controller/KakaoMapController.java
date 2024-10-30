057a47579e00f4fcfad677164e9a985761362554
zerodeok <dudejr3165@naver.com>
2024-10-30 16:48:19 +0900

    feat : 지도 위치정보 저장 기능 구현중 테스트 commit

--- src/main/java/com/sparta/gathering/domain/map/controller/KakaoMapController.java
+++ src/main/java/com/sparta/gathering/domain/map/controller/KakaoMapController.java
@@ -1,16 +1,16 @@
 package com.sparta.gathering.domain.map.controller;
 
-import com.sparta.gathering.common.response.ApiResponse;
+//import com.sparta.gathering.domain.map.service.KakaoService;
 import com.sparta.gathering.domain.map.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import net.minidev.json.parser.ParseException;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 @RequiredArgsConstructor
@@ -19,21 +19,20 @@ public class KakaoMapController {
 
     private final RestTemplate restTemplate;
     private final KakaoService kakaoService;
+//    private final KakaoService kakaoService;
 
+    /**
+     * 카카오맵 지번으로 검색
+     * @param searchMap 검색할 주소
+     */
     @GetMapping("/api/kakaoMap/{searchMap}")
-    public ResponseEntity<String> searchMap(@PathVariable String searchMap) throws UnsupportedEncodingException, URISyntaxException, ParseException {
+    public ResponseEntity<String> searchMap(@PathVariable String searchMap) {
 
-        kakaoService.searchMap();
-        // Set up headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        String appkey = "KakaoAK fe5907af108eb89d33ecb50f39844b0e";//
-        headers.set("Authorization", appkey);
+        return kakaoService.searchMap(searchMap);
+    }
 
-        // Create entity with headers
-        HttpEntity<String> entity = new HttpEntity<String>("parameters",headers);
-        String rawURI = "https://dapi.kakao.com/v2/local/search/address.json?query=" + searchMap;
-
-        return restTemplate.exchange(rawURI, HttpMethod.GET, entity, String.class);
+    @PostMapping("/api/kakaoMap/{saveMap}")
+    public void saveMap(@PathVariable String saveMap) {
+        kakaoService.saveMap(saveMap);
     }
 }
