fac18ff60ef26502912f60b22aaf0a89ff7910e5
LJH <skRtl365@gmail.com>
2024-10-30 23:45:33 +0900

    Fix : Where 어노테이션 deprecated로 인한 변경

--- src/main/java/com/sparta/gathering/domain/category/entity/Category.java
+++ src/main/java/com/sparta/gathering/domain/category/entity/Category.java
@@ -4,20 +4,27 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sparta.gathering.common.entity.Timestamped;
 import com.sparta.gathering.domain.category.dto.request.CategoryReq;
 import com.sparta.gathering.domain.user.entity.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor
 @Table(name = "category")
-@Where(clause = "deleted_at IS NULL")
+@Filter(name = "deleted", condition = "deleted_at IS NULL")
 @SQLDelete(sql = "UPDATE category SET deleted_at = NOW() WHERE id = ?")
 public class Category extends Timestamped {
 
