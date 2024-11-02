9104aa99ba6bd55ddcbee21ecc93c4a06470aa1d
mj-song00 <niveus128@gmail.com>
2024-11-02 21:34:08 +0900

    Fix: @Repository 제거

--- src/main/java/com/sparta/gathering/domain/gather/repository/GatherRepository.java
+++ src/main/java/com/sparta/gathering/domain/gather/repository/GatherRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface GatherRepository extends JpaRepository<Gather, Long> {
+
+public interface GatherRepository extends JpaRepository<Gather, Long>, GatherCustomRepository {
 
     @Query("SELECT DISTINCT g FROM Gather g " +
             "JOIN FETCH g.category c " +
