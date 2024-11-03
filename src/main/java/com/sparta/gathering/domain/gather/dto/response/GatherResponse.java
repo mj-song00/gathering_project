e3d0b03523b2c220641d717660e4893775c21d1f
mj-song00 <niveus128@gmail.com>
2024-11-03 21:10:07 +0900

    Refactor : GatherListResponse ->  GatherResponse 변경

--- src/main/java/com/sparta/gathering/domain/gather/dto/response/GatherResponse.java
+++ src/main/java/com/sparta/gathering/domain/gather/dto/response/GatherResponse.java
@@ -6,33 +6,46 @@ import com.sparta.gathering.domain.hashtag.entity.HashTag;
 import com.sparta.gathering.domain.schedule.entity.Schedule;
 import lombok.Getter;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-public class GatherListResponseItem {
+public class GatherResponse {
 
     private final Long id; // Gather ID
     private final String title; // Gather 제목
     private final String description;
     private final List<String> hashtags;
+    private final List<Long> scheduleIds;
     private final List<String> schedule;
+    private final List<Long> boardIds;
     private final List<String> board;
     private final String category;
     private final String address;
 
 
-    public GatherListResponseItem(Gather gather) {
+    public GatherResponse(Gather gather) {
         this.id = gather.getId(); // Gather ID
         this.title = gather.getTitle(); // Gather 제목
         this.description = gather.getDescription();
         this.hashtags = gather.getHashTagList().stream()
                 .map(HashTag::getHashTagName)
                 .collect(Collectors.toList());
+        this.scheduleIds = gather.getScheduleList().isEmpty() ? null
+                : gather.getScheduleList().stream()
+                .map(Schedule::getId)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
         this.schedule = gather.getScheduleList().stream()
-                .map(Schedule::getScheduleTitle).collect(Collectors.toList());
+                .map(Schedule::getScheduleTitle).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        this.boardIds = gather.getBoardList().isEmpty() ? null
+                : gather.getBoardList().stream()
+                .map(Board::getId)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
         this.board = gather.getBoardList().stream()
-                .map(Board::getBoardTitle).collect(Collectors.toList());
+                .map(Board::getBoardTitle).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         this.category = gather.getCategory().getCategoryName();
         this.address = gather.getMap().getAddressName();
     }
