709760c0ac4720df5da1ed01c532aa7334855827
eunhyeong99 <132648987+eunhyeong99@users.noreply.github.com>
2024-11-04 17:32:24 +0900

    Fix: Conflict Error

--- src/main/java/com/sparta/gathering/domain/gather/entity/Gather.java
+++ src/main/java/com/sparta/gathering/domain/gather/entity/Gather.java
@@ -2,8 +2,8 @@ package com.sparta.gathering.domain.gather.entity;
 
 
 import com.sparta.gathering.common.entity.Timestamped;
-<<<<<<< HEAD
 import com.sparta.gathering.domain.board.entity.Board;
+import com.sparta.gathering.domain.category.entity.Category;
 import com.sparta.gathering.domain.schedule.entity.Schedule;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -13,35 +13,21 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-=======
-import com.sparta.gathering.domain.category.entity.Category;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
->>>>>>> 60c6881a7279a7b423ceb3089bfd2ecd1bf35146
 @Entity
 @NoArgsConstructor
 @Table(name = "gather")
 @Getter
 public class Gather extends Timestamped {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String title;
-  private String description;
+    private String title;
+    private String description;
 
-<<<<<<< HEAD
+
     @OneToMany(mappedBy = "gather", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Board> boardList = new ArrayList<>();
 
@@ -49,35 +35,29 @@ public class Gather extends Timestamped {
     @OneToMany(mappedBy = "gather", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Schedule> scheduleList = new ArrayList<>();
 
-    public Gather(String title){
+    public Gather(String title) {
         this.title = title;
     }
-=======
-  private LocalDateTime deletedAt;
->>>>>>> 60c6881a7279a7b423ceb3089bfd2ecd1bf35146
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "category_id")
-  private Category category;
+    private LocalDateTime deletedAt;
 
-<<<<<<< HEAD
-    public void delete(){
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+    public Gather(String title, String description, Category category) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+    }
+
+    public void updateGatherTitle(String title) {
+        this.title = title;
+    }
+
+    public void delete() {
         this.deletedAt = LocalDateTime.now();
     }
 
-=======
-  public Gather(String title, String description, Category category) {
-    this.title = title;
-    this.description = description;
-    this.category = category;
-  }
-
-  public void updateGatherTitle(String title) {
-    this.title = title;
-  }
-
-  public void delete() {
-    this.deletedAt = LocalDateTime.now();
-  }
->>>>>>> 60c6881a7279a7b423ceb3089bfd2ecd1bf35146
 }
