cdce4ab6a25a825cf55dd1d38ab8678ed8a269cc
zerodeok <dudejr3165@naver.com>
2024-11-15 16:24:00 +0900

    fix : 모임 페이지 front 구현

---
+++ src/main/resources/static/js/findGather.js
@@ -0,0 +1,29 @@
+//
+// async function findGathersLonJsonData(latitude,longitude){
+//
+//     const distance = document.getElementById("distance").value;
+//     const response = await fetch("http://localhost:8080/api/kakaoMap/RedisSearch", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({latitude ,longitude ,distance })
+//     });
+//     const jsonData = await response.json();
+//     console.log(jsonData);
+//     return jsonData;
+// }
+
+async function getAddressLonJsonData(){
+    const address = document.getElementById("address").value;
+    const response = await fetch("http://localhost:8080/api/kakaoMap", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({address})
+    });
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+}
--- src/main/resources/static/js/gather.js
+++ src/main/resources/static/js/gather.js
@@ -4,31 +4,3 @@ async function logJsonData() {
     console.log(jsonData);
     return jsonData;
 }
-async function findGathersLonJsonData(latitude,longitude){
-
-    const distance = document.getElementById("distance").value;
-    const response = await fetch("http://localhost:8080/api/kakaoMap/RedisSearch", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({latitude ,longitude ,distance })
-    });
-    const jsonData = await response.json();
-    console.log(jsonData);
-    return jsonData;
-}
-
-async function getAddressLonJsonData(){
-    const address = document.getElementById("address").value;
-    const response = await fetch("http://localhost:8080/api/kakaoMap", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({address})
-    });
-    const jsonData = await response.json();
-    console.log(jsonData);
-    return jsonData;
-}
