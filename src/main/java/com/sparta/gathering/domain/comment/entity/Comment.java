306df274728ee4d325af2af89c0ed5fdbe0f00af
LJH <skRtl365@gmail.com>
2024-11-18 09:39:05 +0900

    Refactor: 엔티티 매핑 및 스키마 동기화 - Map 엔티티에서 `@OneToOne` 매핑에 `mappedBy` 속성을 추가하여 Gather 엔티티를 연관 관계의 주인으로 설정하여 엔티티 매핑과 데이터베이스 스키마의 불일치 해결 - CommentRepository의 메서드명 오타를 수정하여 `DeleteAt`를 `DeletedAt`로 변경 - Hibernate 스키마 변경 시도를 방지하기 위해 엔티티 매핑과 스키마를 동기화하고 설정을 검토 - Map과 Gather 엔티티 간의 연관 관계 설정 로직을 수정하여 Gather 엔티티에서 Map 엔티티를 관리하도록 변경

--- src/main/java/com/sparta/gathering/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/gathering/domain/comment/entity/Comment.java
@@ -3,10 +3,17 @@ package com.sparta.gathering.domain.comment.entity;
 import com.sparta.gathering.common.entity.Timestamped;
 import com.sparta.gathering.domain.member.entity.Member;
 import com.sparta.gathering.domain.schedule.entity.Schedule;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
 
 @Entity
@@ -26,7 +33,7 @@ public class Comment extends Timestamped {
     private String comment;//댓글
 
     @Column
-    private LocalDateTime deleteAt;
+    private LocalDateTime deletedAt;
 
     @ManyToOne
     @JoinColumn(name = "schedule_Id")
@@ -37,17 +44,19 @@ public class Comment extends Timestamped {
     private Member member;
 
 
-    public Comment(String comment,Schedule schedule,Member member){
+    public Comment(String comment, Schedule schedule, Member member) {
         this.nickName = member.getUser().getNickName();
         this.comment = comment;
         this.schedule = schedule;
         this.member = member;
     }
-    public void update(String comment){
-        this.comment = comment ;
+
+    public void update(String comment) {
+        this.comment = comment;
     }
-    public void delete(){
-        this.deleteAt = LocalDateTime.now();
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
     }
 }
 
