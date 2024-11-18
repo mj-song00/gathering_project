e9d211fe5d80f89e624821491394f8bfc4ff93af
LJH <skRtl365@gmail.com>
2024-11-18 17:43:00 +0900

    test12

--- src/main/java/com/sparta/gathering/common/config/BatchConfig.java
+++ src/main/java/com/sparta/gathering/common/config/BatchConfig.java
@@ -1,11 +1,13 @@
 package com.sparta.gathering.common.config;
 
 import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
+@EnableBatchProcessing
 @Configuration
 public class BatchConfig {
 
