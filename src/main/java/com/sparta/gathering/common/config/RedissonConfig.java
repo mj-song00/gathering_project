77f438dff2fcc157de00d92fa410594bcda43269
gyeol <gyeol9012@gmail.com>
2024-11-22 10:26:22 +0900

    Feat : 본인이 가입된 모임 조회 추가 Refactor : 쿠폰 Rock Redisson 으로 변경 Add : RedissonConfig 추가 Chore : Redisson 의존성 추가

---
+++ src/main/java/com/sparta/gathering/common/config/RedissonConfig.java
@@ -0,0 +1,21 @@
+package com.sparta.gathering.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379")
+                .setConnectionMinimumIdleSize(10)
+                .setConnectionPoolSize(20);
+        return Redisson.create(config);
+    }
+}
+
---
+++ src/main/java/com/sparta/gathering/domain/gather/dto/response/GatherListResponse.java
@@ -0,0 +1,16 @@
+package com.sparta.gathering.domain.gather.dto.response;
+
+import com.sparta.gathering.domain.gather.entity.Gather;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GatherListResponse {
+    private final Long id;
+    private final String title;
+
+    public static GatherListResponse from(Gather gather) {
+        return new GatherListResponse(gather.getId(), gather.getTitle());
+    }
+}
